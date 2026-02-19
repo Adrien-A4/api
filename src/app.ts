066ed52190a130ee98bebdcd20bf16a25fb892fe
AdrienA4 <jinsuga77@gmail.com>
2026-02-19 16:39:06 +0400

    Added a favicon to the API and updated the package.json to include a development script for easier testing.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import routes from "./routes/index.js";
-
+import path from "path";
+import favicon from "serve-favicon";
 const app = express();
 app.use(express.json());
 app.use("/", routes);
 app.get("/", (req, res) => {
   res.json({ message: "Adrien's API is up and running!" });
 });
+const __dirname = path.resolve();
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
 app.use((req, res, next) => {
   res.status(404).json({ error: "Bad Request, try another endpoint." });
 });
